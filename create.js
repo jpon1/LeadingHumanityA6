47910bb6f414e615002e1d77c22059dc64836a3a
Jake Vo <javo@ucsd.edu>
2016-11-10 20:29:26 -0800

    adding create.js

---
+++ create.js
@@ -0,0 +1,36 @@
+
+var array1 = [];
+var pName;
+var topic;
+var quest;
+var hypo;
+var url;
+
+function func1(a,b,c,d,e) {
+
+    pName = a;
+    topic = b;
+    ques = c;
+    hypo = d;
+    url = e;
+};
+
+function getProjectName() {
+  return pName;
+}
+
+function getTopicName() {
+  return topic;
+}
+
+function getQuestion() {
+  return ques;
+}
+
+function getHypo() {
+  return hypo;
+}
+
+function getUrl() {
+  return url;
+}
